42729e03faa048f49773899dfb0e8c34aa72a94b
RESHMA R <reshma24799@gmail.com>
2024-03-26 22:02:00 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 
 //getting error, will fix later
@@ -12,8 +13,10 @@ mongoose.connect('mongodb+srv://reshma24799:Newjob24@mern-blog.x0vficy.mongodb.n
     console.log(error);
 })
 const app = express();
+app.use(express.json()); //allow json a sthe input of backend
 
 app.listen(3000,() => {
     console.log('server is running on port 3000')
 });
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
