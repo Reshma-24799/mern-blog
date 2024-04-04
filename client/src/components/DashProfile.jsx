4f7c97f616506dad676a9dae51b88e128d90aa75
RESHMA R <reshma24799@gmail.com>
2024-04-04 17:19:38 +0530

    create the post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -9,9 +9,10 @@ import { updateStart, updateSuccess, updateFailure, deleteUserStart,
          deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle} from 'react-icons/hi';
+import { Link } from "react-router-dom"
 
 export default function DashProfile() {
-    const { currentUser } = useSelector((state) => state.user);
+    const { currentUser, loading } = useSelector((state) => state.user);
     const[imageFile,setImageFile] = useState(null);
     const[imageFileUrl,setImageFileUrl] = useState(null);
     const[imageFileUploadProgress, setImageFileUploadProgress] =useState(null);
@@ -173,7 +174,17 @@ export default function DashProfile() {
         <TextInput type='text' id='email' placeholder='email' defaultValue={currentUser.email} onChange={handleChange}/>
         <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
 
-        <Button disabled={imageFileUploading } type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
+        <Button disabled={imageFileUploading || loading } type='submit' gradientDuoTone='purpleToBlue' outline>
+          {loading ? 'Loading...' : 'Update'}
+        </Button>
+        { currentUser.isAdmin === true && (
+            <Link to='/create-post'>
+              <Button type='button' gradientDuoTone='purpleToPink' className='w-full'>
+                Create a Post
+              </Button>
+            </Link>
+          )
+        }
       </form>
       <div className='text-red-500 flex justify-between mt-5'>
         <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
