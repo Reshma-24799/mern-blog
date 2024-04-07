03620cc9b0018bfd1d8f7ab754edd8705ee1c88d
RESHMA R <reshma24799@gmail.com>
2024-04-07 14:42:13 +0530

    show post inside dashboard and add show more functionality

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -1,8 +1,109 @@
+import { useEffect, useState } from "react"
+import {useSelector} from 'react-redux';
+import {Table } from 'flowbite-react';
+import { Link } from "react-router-dom";
 
 export default function DashPost() {
+  const { currentUser } = useSelector((state) => state.user);
+  const[userPosts,setUserPosts] = useState([]);
+  const[showMore, setShowMore] =useState(true);
+  console.log(userPosts);
+  useEffect(() => {
+    const  fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getposts?userId=${currentUser._id}`)
+        const data = await res.json();
+        if(res.ok){
+          if(data.posts.length < 9){
+            setShowMore(false);
+          }
+          setUserPosts(data.posts);
+        }
+      } catch (error) {
+        console.log(error.messafe);
+      }
+    }
+    if(currentUser.isAdmin){
+      fetchPosts();
+    }
+  },[currentUser._id]);
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+      const data = await res.json();
+      if(res.ok){
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if(data.posts.length < 9){
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);;
+    }
+  }
   return (
     <div>
-      DashPost
+        {currentUser.isAdmin && userPosts.length > 0 ? (
+            <>
+              <Table hoverable className="shadow-md">
+                <Table.Head>
+                  <Table.HeadCell>Date updated</Table.HeadCell>
+                  <Table.HeadCell>Post image</Table.HeadCell>
+                  <Table.HeadCell>Post title</Table.HeadCell>
+                  <Table.HeadCell>Category</Table.HeadCell>
+                  <Table.HeadCell>Delete</Table.HeadCell>
+                  <Table.HeadCell>
+                    <span>Edit</span>
+                  </Table.HeadCell>
+                </Table.Head>
+                {userPosts?.map((post) => (
+                    <Table.Body className='divide-y'>
+                      <Table.Row>
+                        <Table.Cell>
+                          {new Date(post.updatedAt).toLocaleDateString()}
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Link to={`/post/${post.slug}`}>
+                              <img 
+                                src={post.image} 
+                                alt={post.title}
+                                className="w-20 h-10 object-cover bg-gary-500" 
+                              />
+                          </Link>
+                        </Table.Cell>
+                        <Table.Cell>
+                        <Link className="font-medium text-gray-900 dark:text-white" to={`/post/${post.slug}`}>
+                              {post.title}
+                          </Link>
+                        </Table.Cell>
+                        <Table.Cell>{post.category}</Table.Cell>
+                        <Table.Cell>
+                          <span className="font-medium text-red-500 hover:underline cursor-pointer">
+                              Delete
+                          </span>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <Link className="text-teal-500 hover:underline cursor-pointer" to={`/update-post/${post._id}`}>
+                            <span>Edit</span>
+                          </Link>
+                        </Table.Cell>
+                        
+                      </Table.Row>
+                    </Table.Body>
+                  ))}
+              </Table>
+              {
+                showMore && (
+                  <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+                    Show more
+                  </button>
+                )
+              }
+            </>
+          ):(
+            <p></p>
+          )}
     </div>
   )
 }
