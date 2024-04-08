c03b52fabda000920dbe2ad4e00408a0ebdd8171
RESHMA R <reshma24799@gmail.com>
2024-04-08 21:04:43 +0530

    complete calltoaction functionality

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Link, useParams} from 'react-router-dom'
 import {  Button, Spinner } from 'flowbite-react'
+import CallToAction from '../components/CallToAction';
 export default function PostPage() {
     const {postSlug } = useParams();
     const[loading, setLoading] = useState(true);
-    const[error, setError] = useState(false);
     const[post, setPost] = useState(null);
     useEffect(() => {
         const fetchPost = async () => {
@@ -14,16 +14,13 @@ export default function PostPage() {
                 const data = await res.json();
                 if(!res.ok){
                     setLoading(false);
-                    setError(true);
                     return;
                 } else {
                     setPost(data.posts[0]);
                     setLoading(false);
-                    setError(false)
                 }
             } catch (error) {
                 setLoading(false);
-                setError(true);
             }
         }
         fetchPost();
@@ -42,13 +39,16 @@ export default function PostPage() {
                 {post && post.category}
             </Button>
         </Link>
-        <img src={post && post.image} alt={post && post.category} className='mt-10 p-3 max-h-[500px] w-full object-cover'/>
+        <img src={post && post.image} alt={post && post.category} className='mt-10 p-3 max-h-[600px] w-full object-cover'/>
         <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
             <span className='italic'>{post && (post.content.length / 1000).toFixed(0)} mins read</span>
         </div>
         <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+        </div>
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction />
         </div>
     </main>
   )
