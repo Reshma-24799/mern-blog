a96b072ffb6393a6035c9c6b07566d1d79f65858
RESHMA R <reshma24799@gmail.com>
2024-04-10 22:15:06 +0530

    add edit functionality to comments

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -73,6 +73,13 @@ export default function CommentSection({postId}) {
             console.log(error.message);
         }
     }
+    const handleEdit = async (comment, editedContent) => {
+       setComments(
+            comments.map((c) =>
+                c._id === comment._id ? { ...c, content: editedContent} : c
+        )
+       ) ;
+    }
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -128,7 +135,8 @@ export default function CommentSection({postId}) {
                         <Comment
                         key={comment._id} 
                         comment={comment}
-                        onLike={handleLike}/>
+                        onLike={handleLike}
+                        onEdit={handleEdit}/>
                     ))
                 }
                 </>
