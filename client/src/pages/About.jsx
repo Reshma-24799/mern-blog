83eaec547d0b9e11b01de0a61059aa658f6e3bb1
RESHMA R <reshma24799@gmail.com>
2024-04-13 20:50:16 +0530

    add about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div >
+          <h1 className='text-3xl font-semibold text-center my-7'>ABOUT RESHMA CROTCHET'S</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6 dark:text-white'>
+             <p>
+             In the early days of lockdown, as uncertainty loomed large and anxieties ran high, 
+             I stumbled upon a crochet hook and a skein of yarn tucked away in the depths of my closet.
+              Intrigued by the possibilities they held, I decided to give crochet a try. 
+             Armed with determination and a curious spirit, I turned to the vast expanse of the internet for guidance.
+             </p>
+             <p>
+             With no prior experience in crochet, I turned to YouTube tutorials and blog patterns as my trusted companions
+              on this journey of exploration. From the basics of chain stitches to the intricacies of complex patterns,
+              I immersed myself in a world of stitches, colors, and textures, each loop becoming a testament to my newfound love for the craft.
+             </p>
+             <p>
+             Now, as I look back on my crochet journey, I am filled with gratitude for the solace and inspiration it has brought me durin
+              challenging times. But more than that, I am filled with a desire to share this love and knowledge with others who may be
+               seeking their own creative refuge. 
+             </p>
+             <p>
+             Through this blog, I hope to inspire and empower fellow crafters to embark on their own
+              crochet adventures. Whether you're a beginner taking your first tentative steps or a seasoned enthusiast seeking fresh inspiration,
+            there's a place for you here. Let's unravel the threads of imagination together and weave a tapestry of creativity, one stitch at a time.
+             </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
